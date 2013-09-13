cacf343b4a0140e0e35c4bc8656f0aa665a9ea93
Toby Inkster <mail@tobyinkster.co.uk>
2013-09-13 08:58:07 +0100

    this fixes an occasional bug using Specio, because Specio does not provide an is_subtype_of method

--- lib/MooseX/Storage/Engine.pm
+++ lib/MooseX/Storage/Engine.pm
@@ -327,9 +327,11 @@ sub find_type_handler {
     # 100% ideal though, but until I
     # come up with a decent test case
     # it will do for now.
-    foreach my $type (keys %TYPES) {
-        return $TYPES{$type}
-            if $type_constraint->is_subtype_of($type);
+    if ($type_constraint->can('is_subtype_of') {
+        foreach my $type (keys %TYPES) {
+            return $TYPES{$type}
+                if $type_constraint->is_subtype_of($type);
+        }
     }
 
     # NOTE:
